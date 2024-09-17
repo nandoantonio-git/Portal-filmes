e9b3b692dbe54ae04abdd0ac61ea6d96a5053d1b
nandoantonio-git <fernandoluizsilvaantonio@gmail.com>
2024-09-17 07:50:30 -0300

    Base_React

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider,BrowserRouter, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import Home from './pages/Home.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
 
-{/* Deve ter as rotas:
+const router = createBrowserRouter([
+  {
+    path: '/',element:<App/>,
+    errorElement: <PageNotFound/>,
+    children:[
+      {index:true,path:'/home',element:<Home/>},
+      {path:'filmes',element:<MovieListPage/>},
+      {path:'filmes/:id',element:<MovieDetailPage/>},
+      {path:'genero',element:<GenreListPage/>},
+      {path:'genero/:id',element:<MoviesByGenrePage/>}
+    ]
+  }
+])
+/* Deve ter as rotas:
   home
   filmes
   Detalhes do Filme
   Lista de gêneros
   Filmes por gênero
   Page Not Found
-  */}
+  */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </StrictMode>,
 )
--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom"
+
 export default function MovieDetailPage(){
+    const {id} = useParams();
     return(
         <>
         <h1>Movie Detail Page</h1>
-        {/* Exibe detalhes de um filme específico. */}
+        <p>ID:{id}</p>
         </>
     )
 }
